a1cc6a53cd1c488711585018115209a4f3cc68d0
dsatwar12 <deepaksatwar12@gmail.com>
2024-09-08 13:31:20 +0530

    'l'

---
+++ fullstack/backend/java-language/core_java/src/datatypes/yourOwn/Humans.java
@@ -0,0 +1,13 @@
+package datatypes.yourOwn;
+
+public class Humans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	boolean nose=true;
+	String head="yes";
+	void withhead(){}
+
+}
---
+++ fullstack/backend/java-language/core_java/src/oops/InterfacesTopic.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class InterfacesTopic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
