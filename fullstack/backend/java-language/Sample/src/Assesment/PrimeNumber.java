f7bdefe30aad0dadfc79ea370a312a97ad82ea39
dsatwar12 <deepaksatwar12@gmail.com>
2024-11-30 13:30:49 +0530

    'l'

--- fullstack/backend/java-language/Sample/src/Assesment/PrimeNumber.java
+++ fullstack/backend/java-language/Sample/src/Assesment/PrimeNumber.java
@@ -1,18 +1,33 @@
 package Assesment;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
 
-	public static boolean main(String[] args) {
-		// TODO Auto-generated method stub
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-		
-		int num;
-		if (num<=1) {
-			return false;
-		}
-		
-		
-		
-	}
-
+    // Main method to print prime numbers from 1 to N
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of N: ");
+        int N = scanner.nextInt();
+        
+        System.out.println("Prime numbers from 1 to " + N + " are: ");
+        for (int i = 1; i <= N; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
--- fullstack/frontend/library/ecomerace-react-project/src/api/api.js
+++
@@ -1,4 +0,0 @@
-export const apicaller=async()=>{
-let data= fetch('https://fakestoreapi.com/products')    
-return await (await data).json()
-}
